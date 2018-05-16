45d680377a7faa7b2d449c741440700503d44dc7
Santo Commarato <cylynex@gmail.com>
2018-05-16 09:02:05 -0600

    Setup Head and sprites

---
+++ Assets/Scripts/CentipedeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentipedeController : MonoBehaviour {
+
+
+	
+    void Start() {
+        Debug.Log("centipede initialized");
+    }
+
+
+    void Update() {
+        transform.Translate(Vector3.left * Time.deltaTime);
+    }
+
+}
