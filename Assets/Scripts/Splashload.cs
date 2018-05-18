f83d80a0c924e3623bbb9a16f5eb9cb817402ffa
Santo Commarato <cylynex@gmail.com>
2018-05-18 10:44:40 -0600

    Loadscreen

---
+++ Assets/Scripts/Splashload.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splashload : MonoBehaviour {
+
+    void Start() {
+        StartCoroutine("LoadGame");
+    }
+
+
+    IEnumerator LoadGame() {
+        yield return new WaitForSeconds(18);
+        SceneManager.LoadScene("Menu");
+    }
+}
